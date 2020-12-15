bbd2774ed8045455fca34bf38ee9b03d2374fdbb
Boluwatife Fakorede <bfakorede@avallain.com>
2020-12-15 02:08:43 +0100

    Terraform for creating bastion instance

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,14 +1,20 @@
 terraform {
   backend "s3" {
-    bucket = "paqot-api-tfstate"
-    key = "paqot-api.tfstate"
-    region = "us-east-1"
-    encrypt = true
+    bucket         = "paqot-api-tfstate"
+    key            = "paqot-api.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
     dynamodb_table = "paqot-api-tf-state-lock"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.20.0"
+    }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  version = "~> 3.20.0"
+  region  = "us-east-1"
 }
